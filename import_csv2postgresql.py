e4ef69a8d734e62646aeff9e82fc62bf6639a70d
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2024-09-06 13:24:46 +0700

    import csv to postgresql

--- import_csv2postgresql.py
+++ import_csv2postgresql.py
@@ -5,7 +5,7 @@ df1 = pd.read_csv(r"C:\Users\00320845\OneDrive\share\Datasource\all\revenue\2024
 df2 = pd.read_csv(r"C:\Users\00320845\OneDrive\share\Datasource\all\revenue\REVENUE_NT_REPORT_2023.csv")
 df3 = pd.read_csv(r"C:\Users\00320845\OneDrive\share\Datasource\all\revenue\REVENUE_NT_REPORT_2022.csv")
 
-df1['DATE'] = pd.to_datetime(df1['DATE'], format='%Y/%m/%d')
+df1['DATE'] = pd.to_datetime(df1['DATE'], format='%Y-%m-%d')
 df1['AMOUNT'] = pd.to_numeric(df1['AMOUNT'])
 df1['REVENUE_VALUE'] = pd.to_numeric(df1['REVENUE_VALUE'])
 df1['SUB_PRODUCT_KEY'] = pd.to_numeric(df1['SUB_PRODUCT_KEY'])
